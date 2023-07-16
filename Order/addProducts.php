abd156d87643a3bff52ed99e93bf190b38f7cd0d
Davier Lopez <davierlopez18dl@gmail.com>
2023-07-15 22:37:12 -0500

    actualizacion

--- Order/addProducts.php
+++ Order/addProducts.php
@@ -2,13 +2,9 @@
     include("../config/conexion.php");
 
     $idOrder = $_POST["idOrder"];
-    $category = $_POST["category"];
-    $name = $_POST["name"];
-    $price = $_POST["price"];
-    $orderDetail = $_POST["descripcion"];
     $totalPrice = $_POST["totalPrice"];
 
-    $sql = "INSERT INTO products (IdOrder, IdCategory, ProductName, Price, OrderDetails, TotalPrice) VALUES ('$category', '$name', '$price', '$orderDetail', '$totalPrice')";
+    $sql = "INSERT INTO products (IdOrder, TotalPrice) VALUES ('$idOrder','$totalPrice')";
     $resultado = mysqli_query($conexion, $sql);
     // $resultado = $conexion->query($sql); // Otra forma de hacerlo 
      
